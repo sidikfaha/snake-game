c00029afa524d9a86f144b0120ce895803328dd6
Joe Codeur <joefaha1@gmail.com>
2022-04-17 22:03:05 +0100

    fix: deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
 const path = require("path")
 
 module.exports = {
-  entry: './src/ts/index.ts',
-  mode: "development",
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: ['babel-loader', 'ts-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: './css',
-              name: 'app.css'
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
+    entry: './src/ts/index.ts',
+    mode: "development",
+    devtool: "source-map",
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                use: ['babel-loader', 'ts-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                        loader: "file-loader",
+                        options: {
+                            outputPath: '.',
+                            name: 'app.css'
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
 
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['css-loader']
             }
-          }]
-      },
-      {
-        test: /\.css$/,
-        use: ['css-loader']
-      }
-    ],
-  },
-  resolve: {
-    extensions: ['tsx', '.ts', '.js']
-  },
-  output: {
-    filename: "app.js",
-    path: path.join(__dirname, 'dist'),
-  },
+        ],
+    },
+    resolve: {
+        extensions: ['tsx', '.ts', '.js']
+    },
+    output: {
+        filename: "app.js",
+        path: path.join(__dirname, 'dist'),
+    },
 }
